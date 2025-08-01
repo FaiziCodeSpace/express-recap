a499ed63492903a9fde661f11227e15e20bd81a0
FaiziCodeSpace <faizanwebdev1@gmail.com>
2025-08-01 22:23:24 +0500

    added bcrypt to hash password before storing in the database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,28 @@
-import { DataBase, loadData, saveData } from "../app.js";
+import { loadData, saveData } from "../app.js";
+import bcrypt from "bcryptjs";
+
 
 export const getUsers = (req, res)=>{
   const db = loadData();
   const {id} = req.params;
   const existUser = db.find((user)=>user.id==id);
+  const {name} = existUser;
     if(existUser){
-      return res.json(existUser);
+      return res.json({id, name});
     }else if(!id){
-      res.json(DataBase); 
+      res.json(db); 
     }
     res.status(404).json({message:"User not found!"})
 };
 
-export const postUsers = (req, res)=>{
+export const postUsers = async(req, res)=>{
     const db = loadData(); 
-    const {name, password} = req.body;
+    const {name, email, password} = req.body;
+    const hPass = await bcrypt.hash(password, 10);
     const id = db.length + 1;
-    const newUser = {id, name, password};
+    const newUser = {id, name, email, hPass};
     db.push(newUser);
     saveData(db);
-    res.json(db);
+    res.json(newUser);
 };
-
-
-
-
-
-
 
